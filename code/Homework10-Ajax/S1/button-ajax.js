1562b52aa72e51840b309febba82299812f1e977
AKAGB <374118168@qq.com>
2018-03-28 01:34:00 +0800

    hw10 calc

--- code/Homework10-Ajax/S1/button-ajax.js
+++ code/Homework10-Ajax/S1/button-ajax.js
@@ -1,3 +1,34 @@
 $(function() {
+    var 
+        resultDiv = $('#info-bar'),
+        buttons = $('.button'),
+        num_blocks = $('.request-number'),
+        // 判断是否已经计算过结果
+        calc_flag = true, 
+        // 记录计算结果
+        calc_result = 0;
+    
+    buttons.click(function() {
+        var 
+            info_events = $._data(resultDiv[0], 'events'),
+            allNumbers = _.chain(num_blocks)
+                            .filter(o => o.textContent != "")
+                            .map(o => parseInt(o.textContent))
+                            .value();
+        if (allNumbers.length < 5) {
+            resultDiv.off('click', click_info);
+        }
+        else if (calc_flag && (!info_events || !info_events['click'])) {
+            calc_result = _.sum(allNumbers);
+            resultDiv.click(click_info);
+            calc_flag = false;
+        }
+    });
+    
+    // 点击info块的事件，显示计算结果并灭活
+    function click_info() {
+        $('#info-bar').text(calc_result);
+        $('#info-bar').off('click', click_info);
+    }
     
 });
